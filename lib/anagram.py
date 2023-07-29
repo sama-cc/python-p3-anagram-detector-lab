cfa8c7d2e1f6c1ea604822bdaad5a8c01b792db1
Sam A Carpenter-Camhi <sam.camhi@gmail.com>
2023-07-29 02:21:19 -0400

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+        
+    def match(self, list):
+        word_list = [letter for letter in self.word]
+        
+        def check_match(item):
+            item_list = [letter for letter in item]
+            return "matched" if sorted(word_list) == sorted(item_list) else "not a match"
+      
+        matches = [item for item in list if check_match(item) == "matched"]
+        
+        return matches
